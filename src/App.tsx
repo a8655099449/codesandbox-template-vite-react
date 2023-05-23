17d2f9c5c2a9a4b88d92d68251501429df09e6f4
少林寺铁金刚 <58685483+a8655099449@users.noreply.github.com>
2023-05-23 03:00:45 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>mobx + React</h1>
       <h2>On CodeSandbox!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
